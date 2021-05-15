de280a85b5ed9f7ca68657ea720efffee575d863
ashwin2001-cloud <57358373+ashwin2001-cloud@users.noreply.github.com>
2021-05-15 14:34:08 +0530

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -13,30 +13,30 @@ const accessLogStream= rfs.createStream('access.log', {
     path: logDirectory
 });
 
-const development= {
-    name: 'development',
-    asset_path: '/assets',
-    session_cookie_key: "blahsomething",
-    db: 'codial_db',
-    smtp: {
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.CODIAL_GMAIL_USERNAME,
-            pass: process.env.CODIAL_GMAIL_PASSWORD
-        }
-    },
-    google_client_id: process.env.CODIAL_GOOGLE_CLIENT_ID,
-    google_client_secret: process.env.CODIAL_GOOGLE_CLIENT_SECRET,
-    google_callback_URL: "http://codialashwin.tech/users/auth/google/callback",
-    jwt_secret: "codial",
-    morgan: {
-        mode: 'dev',
-        options: {stream: accessLogStream}
-    }
-}
+// const development= {
+//     name: 'development',
+//     asset_path: '/assets',
+//     session_cookie_key: "blahsomething",
+//     db: 'codial_db',
+//     smtp: {
+//         service: 'gmail',
+//         host: 'smtp.gmail.com',
+//         port: 587,
+//         secure: false,
+//         auth: {
+//             user: process.env.CODIAL_GMAIL_USERNAME,
+//             pass: process.env.CODIAL_GMAIL_PASSWORD
+//         }
+//     },
+//     google_client_id: process.env.CODIAL_GOOGLE_CLIENT_ID,
+//     google_client_secret: process.env.CODIAL_GOOGLE_CLIENT_SECRET,
+//     google_callback_URL: "http://codialashwin.tech/users/auth/google/callback",
+//     jwt_secret: "codial",
+//     morgan: {
+//         mode: 'dev',
+//         options: {stream: accessLogStream}
+//     }
+// }
 
 const production= {
     name: 'production',
